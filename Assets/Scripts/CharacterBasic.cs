309cf62d0e7660c8621e9dc6e0e453098d81e909
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-04-29 22:17:54 +0100

    AddUI

--- Assets/Scripts/CharacterBasic.cs
+++ Assets/Scripts/CharacterBasic.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class CharacterBasic : MonoBehaviour
 {
-    
+    public Transform tfCharacter;
+
+    private void FixedUpdate()
+    {
+        float moveRate = Time.deltaTime * 0.5f;
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            tfCharacter.transform.Translate(Vector3.right * moveRate);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            tfCharacter.transform.Translate(Vector3.left * moveRate);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            tfCharacter.transform.Translate(Vector3.forward * moveRate);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            tfCharacter.transform.Translate(Vector3.back * moveRate);
+        }
+    }
+
+
 }
